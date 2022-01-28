b17b2dd57bf1773f6a41edffbf6d5e36360ad9de
Sofyan1965 <94955101+fiyan123@users.noreply.github.com>
2022-01-27 17:19:29 -0800

    Add files via upload

---
+++ forwhile.php
@@ -0,0 +1,10 @@
+<?php
+
+        $a = 0;
+        while($a <=10){         // KONDISI
+            for($i = 0; $i <= 10; $i++){  
+                echo "<p>Perulangan ke ($a , $i)</p>"; // STATEMENT
+            }
+            $a++;       // INCREMENT
+        }
+?>
